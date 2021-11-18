1723aafa35e25dac1e9d4249710051a46ab29a88
Rory <rory.dwy@gmail.com>
2021-11-18 11:39:08 -0800

    Set up initial structor and tools

---
+++ src/Board.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export class DeepSeaAdventureBoard extends React.Component {
+  render() {
+    return (
+      <div id="board">
+        <h1>Hello World</h1>
+      </div>
+    );
+  }
+}
